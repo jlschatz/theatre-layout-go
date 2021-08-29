de72d47f0fe7a9049a306fd035ccc17be1993e30
Jurgen Schatz <Jurgen>
2021-08-29 22:03:41 +0200

    simulate input

--- handlers.go
+++ handlers.go
@@ -26,7 +26,7 @@ func getLayout(w http.ResponseWriter, req *http.Request) {
 	srv := newService()
 
 	var response Resposne
-	response.Layout = srv.seatPeople(4, []int{4, 1, 5, 1, 6, 3, 3, 4, 8, 9})
+	response.Layout = simulateInput(srv)
 
 	b, err := json.Marshal(response)
 	if err != nil {
@@ -36,4 +36,22 @@ func getLayout(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(b)
+}
+
+func simulateInput(srv *service) Layout {
+
+	srv.seatPeople(4, []int{4, 1, 5, 1, 6, 3, 3, 4, 8, 9})
+
+	var testResponse Layout
+	var testSection Section
+	var testRank Rank
+
+	testSection.Name = "hall"
+	testRank.Number = srv.rank.Number
+	testRank.Rows = srv.Layout
+
+	testSection.Ranks = append(testSection.Ranks, testRank)
+	testResponse.Sections = append(testResponse.Sections, testSection)
+
+	return testResponse
 }
--- service.go
+++ service.go
@@ -21,23 +21,11 @@ func newService() *service {
 	return srv
 }
 
-func (s *service) seatPeople(rankNo int, groups []int) Layout {
+func (s *service) seatPeople(rankNo int, groups []int) {
 	s.rank.Number = rankNo
 	sort.Ints(groups)
 	s.creatRow(groups)
 
-	var testResponse Layout
-	var testSection Section
-	var testRank Rank
-
-	testSection.Name = "hall"
-	testRank.Number = s.rank.Number
-	testRank.Rows = s.Layout
-
-	testSection.Ranks = append(testSection.Ranks, testRank)
-	testResponse.Sections = append(testResponse.Sections, testSection)
-
-	return testResponse
 }
 
 func (s *service) creatRow(groups []int) {
